9bd44ee93209ab27127f9153c8bc0c9b90bad49a
Pontus Hedman <phedman@perle.com>
2024-10-24 10:33:54 -0400

    Update rundocker.sh noninteractive behaviour

--- rundocker.sh
+++ rundocker.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-# Run interactive shell by default, else supplied commands
-test $# = 0 && set -- /bin/bash -i
+if test $# = 0; then
+    # No args so run interactive shell by default.
+    # Else, run supplied command(s) noninteractively.
+    set -- /bin/bash -i
+    DFLAGS='-it'
+fi
 
-docker run --rm -it --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 \
+docker run --rm $DFLAGS --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 \
   -h vyos-build \
   -v $(pwd):/vyos -v /dev:/dev -v /etc/fstab:/etc/fstab \
   -v "$HOME/.gitconfig":/etc/gitconfig -w /vyos \
