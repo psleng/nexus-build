5bc43ba8f4c84364d70609304680e97ec8949405
Pontus Hedman <phedman@perle.com>
2024-10-31 15:37:41 -0400

    NXP driver updates

--- build_iGOS_TMDS64EVM_fs.sh
+++ build_iGOS_TMDS64EVM_fs.sh
@@ -173,6 +173,9 @@ if [ ! -f "$BLT" ]; then
         -e 's/MaxLevelSyslog=debug/MaxLevelsyslog=info/' \
             $FS/etc/systemd/journald.conf
 
+    # NXP driver additions to fs
+    tar -C $FS -zxf $ROOTDIR/vyos-build/scripts/package-build/linux-kernel/build_iGOS_drivers/fs.tar.gz
+
     # Decompress the vmlinuz (symlink to the real thing) into Image
     gunzip < build/fs/boot/vmlinuz > build/fs/boot/Image
 
