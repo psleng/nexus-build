734ad7828a544aaea0d5d7ccac8fc89d3380f8f8
Pontus Hedman <phedman@perle.com>
2024-11-04 12:16:32 -0500

    Revamp to maintain user supplied --include order.
    If --include not used, all directories will be processed in alphabetical
    order (minus any --exclude list)

--- package-build.py
+++ package-build.py
@@ -6,13 +6,19 @@ import argparse
 import logging
 
 # Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Set up argument parser
-parser = argparse.ArgumentParser(description='Run build.py in subdirectories, with options to exclude or include specific directories.')
-parser.add_argument('--exclude', nargs='*', default=[], help='List of directories to exclude')
-parser.add_argument('--include', nargs='*', default=[], help='List of directories to only include')
-parser.add_argument('--dir', required=True, help='Directory name "foo" in vyos-build/scripts/foo')
+parser = argparse.ArgumentParser(description='Run build.py in subdirectories,'
+    ' with options to exclude or include specific directories. If --include'
+    ' not used, all directories in DIR are processed alphabetically.')
+parser.add_argument('--exclude', nargs='*', default=[],
+                    help='List of directories to exclude')
+parser.add_argument('--include', nargs='*', default=[],
+                    help='List of directories to only include (in order)')
+parser.add_argument('--dir', required=True,
+                    help='Directory name "foo" in vyos-build/scripts/foo')
 args = parser.parse_args()
 
 # Change directory to the specified directory
@@ -23,47 +29,42 @@ except FileNotFoundError:
     exit(1)
 
 # Get the current working directory
-current_dir = os.getcwd()
-
-# List all items in the current directory and sort them alphabetically
-items = sorted(os.listdir(current_dir))
-
-# Combine exclude lists
-exclude_list = set(args.exclude)
+basedir = os.getcwd()
 
 # Determine directories to process
 if args.include:
-    directories_to_process = set(args.include)
+    # Only these (in listed order)
+    directories_to_process = args.include
 else:
-    directories_to_process = {item for item in items if os.path.isdir(os.path.join(current_dir, item)) and item not in exclude_list}
+    # Use all items in the current directory, alphabetically
+    directories_to_process = sorted(os.listdir(basedir))
 
-# Process in alphabetical order.  Technically the order should not matter,
-# but if we do not sort they run in a different random order every build
-# which makes debugging confusing.
-for item in sorted(directories_to_process):
-    item_path = os.path.join(current_dir, item)
-    
-    # Check if the item is a directory
-    if os.path.isdir(item_path):
-        # Change to the subdirectory
-        os.chdir(item_path)
-        
-        # Construct the path to build.py
-        build_script = os.path.join(item_path, 'build.py')
-        
-        # Check if build.py exists in the directory
-        if os.path.isfile(build_script):
-            # Run build.py and handle errors
-            logging.info(f"Running build.py in {item_path}")
-            try:
-                subprocess.run(['python3', build_script], check=True)
-                logging.info(f"Successfully ran build.py in {item_path}")
-            except subprocess.CalledProcessError as e:
-                logging.error(f"Error running build.py in {item_path}: {e}")
-                exit(2)
-        else:
-            logging.warning(f"No build.py found in {item_path}")
-        
-        # Change back to the original directory
-        os.chdir(current_dir)
+# Process
+for item in directories_to_process:
+    if item in args.exclude:
+        continue
 
+    os.chdir(basedir)
+    item_path = os.path.join(basedir, item)
+    if not os.path.isdir(item_path):
+        continue
+
+    # Change to the subdirectory
+    os.chdir(item_path)
+
+    # Construct the path to build.py
+    buildpy = 'build.py'
+    build_script = os.path.join(item_path, buildpy)
+
+    # Run build.py if it exists
+    if os.path.isfile(build_script):
+        # Run build.py and handle errors
+        logging.info(f"Running {buildpy} in {item_path}")
+        try:
+            subprocess.run(['python3', build_script], check=True)
+            logging.info(f"Successfully ran {buildpy} in {item_path}")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error running {buildpy} in {item_path}: {e}")
+            exit(2)
+    else:
+        logging.warning(f"No {buildpy} found in {item_path}; skipping")
