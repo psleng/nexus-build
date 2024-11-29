510370041fc51600dba5034731efb134c32a07e7
Pontus Hedman <phedman@perle.com>
2024-11-29 09:44:54 -0500

    Unified rundocker.sh
    DTRT depending on platform.

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ARCH=$(arch)
+
 if test $# = 0; then
     # No args so run interactive shell by default.
     # Else, run supplied command(s) noninteractively.
@@ -15,9 +17,15 @@ else
     GITMNT=''
 fi
 
+TAG=current-arm64
+if [ $ARCH != 'aarch64' ]; then
+    # Different tag for non-ARM.  This will hopefully go away.
+    TAG=${TAG}v8
+fi
+
 docker run --rm $DFLAGS --platform linux/arm64 \
   --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 \
   -h vyos-build \
   -v $(pwd):/vyos -v /dev:/dev -v /etc/fstab:/etc/fstab \
   $GITMNT -w /vyos \
-  vyos/vyos-build:current-arm64v8 "$@"
+  vyos/vyos-build:$TAG "$@"
