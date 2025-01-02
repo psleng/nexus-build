108b004825e0aeea29587ed89884446557bbd8ce
psleng <psleng@perle.com>
2025-01-02 18:08:27 -0500

    daily/nightly script for building the latest source and updating the deb repo is needed

---
+++ iGOS-daily-build.sh
@@ -0,0 +1,13 @@
+echo 'Starting daily iGOS build...' > iGOS-build.out
+date >> iGOS-build.out
+cd  nexus-build
+git pull
+make clean
+make all
+cd ..
+ls -l .*built >>  iGOS-build.out
+date >> iGOS-build.out
+echo 'Daily iGOS build completed! Please check *.ERR files for errors.'  >> iGOS-build.out
+echo 'Checking iGOS binary repository for changes and updating if different.' > iGOS-deb-repo.out
+./check-update-apt-repo.sh --repo git@github.com:psleng --clean >> iGOS-deb-repo.out
+
