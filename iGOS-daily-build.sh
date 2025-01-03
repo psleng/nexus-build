9f3e7a83096fda2c3a3e934a71e8e592dc1ad712
psleng <psleng@perle.com>
2025-01-03 10:31:51 -0500

    tweaks  to nightly builds for deb repo logic

--- iGOS-daily-build.sh
+++ iGOS-daily-build.sh
@@ -1,4 +1,3 @@
-cd  nexus-build
 echo 'Starting daily iGOS build...' > iGOS-build.out
 date >> iGOS-build.out
 git pull
@@ -10,5 +9,4 @@ date >> iGOS-build.out
 echo 'Daily iGOS build completed! Please check *.ERR files for errors.'  >> iGOS-build.out
 echo 'Checking iGOS binary repository for changes and updating if different.' > iGOS-deb-repo.out
 ./check-update-apt-repo.sh --repo git@github.com:psleng >> iGOS-deb-repo.out
-cd ..
 
