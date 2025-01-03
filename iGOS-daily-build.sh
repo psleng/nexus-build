54fadb17ee2c3a63565ecd91c12218798a9d8904
psleng <psleng@perle.com>
2025-01-03 18:38:24 -0500

    do a clean of the deb repo and get the latest

--- iGOS-daily-build.sh
+++ iGOS-daily-build.sh
@@ -8,5 +8,5 @@ ls -l .*built >>  iGOS-build.out
 date >> iGOS-build.out
 echo 'Daily iGOS build completed! Please check *.ERR files for errors.'  >> iGOS-build.out
 echo 'Checking iGOS binary repository for changes and updating if different.' > iGOS-deb-repo.out
-./check-update-apt-repo.sh --repo git@github.com:psleng >> iGOS-deb-repo.out
+./check-update-apt-repo.sh --repo git@github.com:psleng --clean >> iGOS-deb-repo.out
 
