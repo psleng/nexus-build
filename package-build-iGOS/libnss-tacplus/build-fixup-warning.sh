4e8605269821f1eaf028adbc835fea82446f7eb3
psleng <psleng@debian12.perle.com>
2025-02-06 10:36:26 -0500

    B12006: cleanup / deprecating building tacacs libs for tacacs support

--- package-build-iGOS/libnss-tacplus/build-fixup-warning.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-sudo sed -i 's/Werror/Wno-address -Wno-stringop-truncation/' configure.ac
-
--- package-build-iGOS/libpam-tacplus/build-install-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-sudo apt-get install -y autoconf-archive libaudit-dev
-sudo dpkg -i ../../libtacplus-map/libtacplus-map1_*.deb
-sudo dpkg -i ../../libtacplus-map/libtacplus-map-dev_*.deb
