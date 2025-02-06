e19abbda78b59ca2c2f2a80f2a7263e9307a6bc5
psleng <psleng@debian12.perle.com>
2025-02-06 10:26:35 -0500

    B12006: use upstream tacacs build package.toml for tacacs support

--- build_iGOS_TMDS64EVM_fs.sh
+++ build_iGOS_TMDS64EVM_fs.sh
@@ -81,7 +81,7 @@ if [ ! -f "$BLT" ]; then
         libvyosconfig vyatta-cfg vyos-http-api-tools vyos-utils \
         ipaddrcheck udp-broadcast-relay hvinfo vyatta-wanloadbalance \
         libmnl libpam-radius-auth initramfs-tools igmpproxy libnss-mapuser \
-        libtacplus-map libpam-tacplus libnss-tacplus
+        tacacs
 
     touch "$BLT" # build success
 else
