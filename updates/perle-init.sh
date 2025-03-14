73948d6bf36f24003e2da03c06da5a9df27cfbde
Pontus Hedman <phedman@perle.com>
2025-03-14 14:37:27 -0400

    live-config build-time emulation.
    Instead of passing in "boot=live" to the kernel command line
    which effectively makes /lib/live/config/* scripts run via
    the live-config.service , do it at buildtime via chroot to
    avoid slowing down the boot.
    
    Beware that /lib/live/boot/* scripts, which are supposed to run early
    and really do need a running system, are still not handled so we still          need updates/perle-init.sh .  It just does not need to fake up the
    /lib/live/config/* actions.
    
    Note that VyOS itself relies on seeing the pattern "BOOT_IMAGE=/boot/"
    at the start of /proc/cmdline in order to adjust itself for "image install"
    behaviour (which is ultimately also what we want).  However, their code
    assumes the boot was via grub, so if it was not (normal for our arm64 boards)
    it breaks bootup.  This will have to be readdressed later.

--- updates/perle-init.sh
+++ updates/perle-init.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
-#
+
+### /lib/live/boot/* functionality
+
+# === Corresponds to lib/live/boot/9990-vyos.sh
+# create the /config mount point to /opt/vyatta/etc/config and set uid/group root:vyattacfg
+	chown root:vyattacfg /config
+	mount --bind /opt/vyatta/etc/config /config
+#	chown root:vyattacfg /config
+
+
+### /lib/live/config/* functionality
+# None of these should be needed if updates/run-live-config
+# is run during the build.
+
+# === Corresponds to lib/live/config/1090-ssl-cert
 # check if snakeoil private and public keys exist, and create if either is absent
 #
 	if [ ! -e /etc/ssl/certs/ssl-cert-snakeoil.pem ] || [ ! -e /etc/ssl/private/ssl-cert-snakeoil.key ]
@@ -9,10 +23,4 @@
 
 	# Creating state file
 	touch /var/lib/live/config/ssl-cert
-#
-# create the /config mount point to /opt/vyatta/etc/config and set uid/group root:vyattacfg
-#
-	chown root:vyattacfg /config
-	mount --bind /opt/vyatta/etc/config /config
-#	chown root:vyattacfg /config
 
