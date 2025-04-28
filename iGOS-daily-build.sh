ff6d06d12ccbe9b614870f3fbc9851fc597d0378
Pontus Hedman <phedman@perle.com>
2025-04-28 13:21:59 -0400

    Cleanup.
    Affects automatic daily build only.

--- iGOS-daily-build.sh
+++ iGOS-daily-build.sh
@@ -1,24 +1,29 @@
+#!/bin/sh
 sudo rm -rf nexus-build
 git clone https://github.com/psleng/nexus-build
 cd nexus-build
-echo 'Starting daily iGOS build...' > iGOS-build.out
+
+exec > iGOS-build.out 2>&1
+echo 'Starting daily iGOS build...'
 make spotless
-date >> iGOS-build.out
-echo 'Starting targ-ti-j7200 iGOS build...' >> iGOS-build.out
-date >> iGOS-build.out
+date
+echo 'Starting targ-ti-j7200 iGOS build...'
+date
 make targ-ti-j7200
 make all
-echo 'Finished targ-ti-j7200 iGOS build...' >> iGOS-build.out
-date >> iGOS-build.out
-ls -l .*built >>  iGOS-build.out
+echo 'Finished targ-ti-j7200 iGOS build...'
+date
+ls -l .*built
 make mostlyclean
-echo 'Starting targ-ti-am64x iGOS build...' >> iGOS-build.out
-date >> iGOS-build.out
+echo 'Starting targ-ti-am64x iGOS build...'
+date
 make targ-ti-am64x
 make all
-ls -l .*built >>  iGOS-build.out
-date >> iGOS-build.out
-echo 'Daily iGOS build completed! Please check *.ERR files for errors.'  >> iGOS-build.out
-echo 'Checking iGOS binary repository for changes and updating if different.' > iGOS-deb-repo.out
-./check-update-apt-repo.sh --repo git@github.com:psleng --clean >> iGOS-deb-repo.out
-cd -
+ls -l .*built
+date
+echo 'Daily iGOS build completed! Please check *.ERR files for errors.'
+
+exec > iGOS-deb-repo.out 2>&1
+echo 'Checking iGOS binary repository for changes and updating if different.'
+./check-update-apt-repo.sh --repo git@github.com:psleng --clean
+echo status=$?
