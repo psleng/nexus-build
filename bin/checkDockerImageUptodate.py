82f65d63e49c04135d154a3dbb06c8e1a8c0145b
Pontus Hedman <phedman@perle.com>
2025-07-10 13:50:28 -0400

    ssh support for rundocker.sh
    This will supply the ~/.ssh credentials of the user invoking
    rundocker.sh to the container process.
    
    This is so that we can start cloning https://github.com/Perle-Systems-Limited/
    repositories in the build.  This is useful for proprietary code which
    we do not want public via https://github.com/psleng .
    
    NOTE: Needs commit 0f083a05538f820487670a12375cd57ac68fdf52
    from vyos-build to work.  The docker image needs to be rebuilt
    with that commit checked out.

--- bin/checkDockerImageUptodate.py
+++ bin/checkDockerImageUptodate.py
@@ -5,7 +5,7 @@ import sys
 from subprocess import run
 
 
-def main(img: str, dockerfile: str) -> int:
+def main(img: str, dockerfile: str, entrypoint: str) -> int:
     '''
     Compare docker image "img" to dockerfile "dockerfile".
     Returns 0 if the image is newer than the dockerfile, else 1.
@@ -24,27 +24,27 @@ def main(img: str, dockerfile: str) -> int:
         imgst = time.strptime(imgdate, '%Y-%m-%d %H:%M:%S %z %Z')
         img_mtime = time.mktime(imgst)
 
-    # Get timestamp of the Dockerfile-*
-    df_mtime = Path(dockerfile).stat().st_mtime
+    # Check paths up to date compared to image
+    for f in (dockerfile, entrypoint):
+        # Get timestamp
+        df_mtime = Path(f).stat().st_mtime
+        if (img_mtime - df_mtime) > 0:
+            needs = "does NOT need"
+            ret = 0
+        else:
+            print(f'{f} is newer than docker image')
+            needs = "DOES need"
+            ret = 1
 
-    # print(time.ctime(img_mtime))
-    # print(time.ctime(df_mtime))
-    # print(img_mtime - df_mtime)
-
-    if (img_mtime - df_mtime) > 0:
-        needs = "does NOT need"
-        ret = 0
-    else:
-        needs = "DOES need"
-        ret = 1
     print(f'docker image {needs} to be rebuilt')
 
     return ret
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print(f'Usage {sys.argv[0]} imgname Dockerfile', file=sys.stderr)
+    if len(sys.argv) != 4:
+        print(f'Usage: {sys.argv[0]} imgname Dockerfile entrypoint.sh',
+              file=sys.stderr)
         exit(1)
 
-    exit(main(sys.argv[1], sys.argv[2]))
+    exit(main(sys.argv[1], sys.argv[2], sys.argv[3]))
--- rundocker.sh
+++ rundocker.sh
@@ -13,13 +13,22 @@ else
     fi
 fi
 
+# Make invoking user's .gitconfig available
 GITCONFIG=$HOME/.gitconfig
 if [ -f $GITCONFIG ]; then
     GITMNT="-v $GITCONFIG:/etc/gitconfig"
 else
     echo "$0: WARNING: \"$GITCONFIG\" does not exist." >&2
-    echo "$0: Copy your personal ~/.gitconfig to that location" >&2
     GITMNT=''
+fi
+
+# Make invoking user's .ssh/ available
+SSH=$HOME/.ssh
+if [ -d $SSH ]; then
+    SSHMNT="-v $SSH:/etc/.ssh"
+else
+    echo "$0: WARNING: \"$SSH\" does not exist." >&2
+    SSHMNT=''
 fi
 
 . ./.defs.mk
@@ -38,5 +47,5 @@ docker run --rm $DFLAGS \
   --privileged --sysctl net.ipv6.conf.lo.disable_ipv6=0 \
   -h vyos-build \
   -v $(pwd):/vyos -v /dev:/dev -v /etc/fstab:/etc/fstab \
-  $GITMNT -w /vyos \
+  $GITMNT $SSHMNT -w /vyos \
   vyos/vyos-build:$TAG "$@"
