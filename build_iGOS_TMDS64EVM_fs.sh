d9d1984ecd79454cd09b98b5cd727d69946357af
johnlfeeney <johnlfeeney@gmail.com>
2026-07-01 12:00:57 -0400

    Build fixes

--- build_iGOS_TMDS64EVM_fs.sh
+++ build_iGOS_TMDS64EVM_fs.sh
@@ -213,7 +213,12 @@ if [ ! -f "$BLT" ]; then
     if [ "$BUILDTARG" = "x86_64" ]; then
         BUILDFLAVOUR=generic
     else
-        BUILDFLAVOUR=${ARCH}fs
+        case "$BUILDTYPE" in
+            bookworm-am64xx-iolan) BUILDFLAVOUR=igos-am64x-all ;;
+            bookworm-am64xx-evm)   BUILDFLAVOUR=igos-am64x-evm ;;
+            bookworm-j7200-evm)    BUILDFLAVOUR=igos-j7200-evm ;;
+            *) echo "=== E: unknown BUILDTYPE: $BUILDTYPE" >&2; exit 1 ;;
+        esac
     fi
     export VYOS1X_REPO_URL=https://github.com/psleng/vyos-1x
     export VYOS1X_REPO_BRANCH=psl-master
@@ -287,8 +292,8 @@ if [ ! -f "$BLT" ]; then
 
     if [ "$BUILDTYPE" = "bookworm-am64xx-iolan" ]; then
         # Copy for early-gpio-init service
-        sudo cp updates/perle_gpio_map.py $FS/usr/local/bin
-        sudo cp updates/perle_gpioctl.py $FS/usr/local/bin
+#        sudo cp updates/perle_gpio_map.py $FS/usr/local/bin
+#        sudo cp updates/perle_gpioctl.py $FS/usr/local/bin
 #        sudo cp updates/early-gpio-init.service $FS/etc/systemd/system
 #        sudo ln -s /etc/systemd/system/early-gpio-init.service $FS/etc/systemd/system/sysinit.target.wants/early-gpio-init.service
 
