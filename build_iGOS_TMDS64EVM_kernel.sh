0838dffe2970eca1c995822842c1fa79571243f4
johnlfeeney <johnlfeeney@gmail.com>
2026-07-02 11:40:58 -0400

    And make from my own repo - need to fix this  someday

--- build_iGOS_TMDS64EVM_kernel.sh
+++ build_iGOS_TMDS64EVM_kernel.sh
@@ -32,7 +32,7 @@ fi
 
 # Clone the repository if it doesn't exist or was cleaned
 if [ ! -d "$REPO_NAME" ]; then
-    git clone --single-branch "$REPO_URL"
+    git clone -b vyos-build-jf --single-branch "$REPO_URL"
 fi
 
 # Make Kernel and associated packages
