d4c05329cbf1ff1b53d6aebc7c5719e5ce5901ea
johnlfeeney <johnlfeeney@gmail.com>
2026-07-22 16:51:30 -0400

    Modified so i can build just what i need

--- build_iGOS_TMDS64EVM_fs.sh
+++ build_iGOS_TMDS64EVM_fs.sh
@@ -36,8 +36,8 @@ fi
 
 # Clone the repository if it doesn't exist or was cleaned
 if [ ! -d "$REPO_NAME" ]; then
-    git clone -b psl-master --single-branch "$REPO_URL"
-#    git clone -b vyos-build-jf --single-branch "$REPO_URL"
+#    git clone -b psl-master --single-branch "$REPO_URL"
+    git clone -b vyos-build-jf --single-branch "$REPO_URL"
 fi
 
 # Copy package-build-iGOS/ to vyos-build/scripts/ now that it's there
